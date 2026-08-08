e6b8d76b1136bce73b23fa1e7067342f2894ed53
Student-Ronewa <st10157545@rcconnect.edu.za>
2026-08-08 20:22:07 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace st10157545_abcretail.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
